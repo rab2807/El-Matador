63fedabb11a1dfbe7a8357d5cb11d893a8735533
rab2807 <76392266+rab2807@users.noreply.github.com>
2023-01-05 03:02:29 +0600

    added villain trigger

---
+++ Assets/Scripts/Gameplay/VillainTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillainTrigger : MonoBehaviour
+{
+    private Villain villain;
+     void Start()
+     {
+         villain = transform.parent.gameObject.GetComponent<Villain>();
+     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            villain.InChargingPhase = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            villain.InChargingPhase = false;
+        }
+    }
+}
